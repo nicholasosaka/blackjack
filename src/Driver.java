644412e97b28d57743d0564d935774e922c2c7b7
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-10 14:11:48 -0400

    modify Driver class to remove test information

--- src/Driver.java
+++ src/Driver.java
@@ -3,25 +3,10 @@ import java.util.ArrayList;
 public class Driver {
 	public static void main(String[] args){
 
-		Card ace = new Card(0);
-		Card ten = new Card(9);
-		Card king = new Card(12);
-
-		System.out.println(ace);
-		System.out.println(ten);
-		System.out.println(king);
-
+		Deck deck = new Deck();
 		Hand hand = new Hand();
 
-		hand.hit(ace);
-		System.out.println("Hit ace: " + hand.getValue());
 
-		hand.hit(ten);
-		System.out.println("Hit ten: " + hand.getValue());
 
-		hand.hit(ace);
-		System.out.println("Hit ace: " + hand.getValue());
-
-		System.out.println(hand.toString());
 	}
 }
