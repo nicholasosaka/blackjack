5959a9ea00b9c677c53eef90504d81d2b5196392
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-10 18:46:32 -0400

    Player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,42 @@
+import java.util.ArrayList;
+
 public abstract class Player {
+
+	private String name;
+	private int bank;
+	private Hand hand;
+
+	Player(){
+		bank = 1000;
+	}
+
+	Player(int money){
+		this.bank = money;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getMoney() {
+		return bank;
+	}
+
+	public void addMoney(int money) {
+		this.bank += money;
+	}
+
+	public void removeMoney(int money){
+		this.bank -= money;
+	}
+
+	public void hit(Card card){
+		hand.hit(card);
+	}
+
+	public ArrayList<Card> dump(){
+		return hand.dump();
+	}
+
+
 }
