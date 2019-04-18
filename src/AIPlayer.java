f20aa95ccc2229114c332f8b5fe2ec621e34cb5e
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-17 20:48:08 -0400

    implement AI turns

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -26,7 +26,44 @@ public class AIPlayer extends Player {
 	 */
 	@Override
 	public boolean playTurn(){
-		//TODO implement AI for turn playing
+		Card toDeal;
+
+
+		while(this.isPlayable()){
+			if(this.getHand().getValue() <= 10){ //double if hand is less than or equal to 10
+				toDeal = Table.deck.deal();
+				System.out.println(this.getName() + " doubled and was dealt " + toDeal.toString());
+
+				hit(toDeal);
+
+				int previousBetAmount = this.getBetAmount();
+				this.addMoney(previousBetAmount);  //reset bet to allow for doubling bet amount
+				this.setBetAmount(2*previousBetAmount);
+
+				System.out.println(this.getName() + "'s current hand is " + this.getHand().toString());
+				this.setPlayable(false);
+
+			} else if (this.getHand().getValue() < 16){ //hit if less than 15
+				toDeal = Table.deck.deal();
+				System.out.println(this.getName() + " hit and was dealt " + toDeal.toString());
+
+				hit(toDeal);
+
+				System.out.println(this.getName() + "'s hand is now " + this.getHand().toString());
+
+			} else if (this.getHand().getValue() <= 21){    //stand otherwise, provided it's not a bust
+
+				System.out.println(this.getName() + " stood.");
+				this.setPlayable(false);
+			}
+
+			if(this.getHand().getValue() > 21){
+				this.setPlayable(false);
+				System.out.println("Bust! No payout.");
+			}
+		}
+
+
 
 		return false;
 	}
