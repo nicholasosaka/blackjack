5dac2853192ccdf6226d7956808990f82d348cf8
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-20 09:02:48 -0400

    make better tests

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -1,35 +1,33 @@
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.Random;
-
 public class CardTest {
-	private static Random rand = new Random(System.currentTimeMillis());
 	private static Card a;
 	private static Card b;
 
-	@BeforeClass
-	public static void setUp() {
-		a = new Card(rand.nextInt(53));
-		b = new Card(rand.nextInt(53));
+
+	@Test
+	public void compareToLesser() {
+		a = new Card(Card.Suits.SPADES, Card.Ranks.TWO);
+		b = new Card(Card.Suits.SPADES, Card.Ranks.JACK);
+
+		Assert.assertEquals(-9,a.compareTo(b));
 	}
 
 	@Test
-	public void compareTo() {
+	public void compareToEqual() {
+		a = new Card(Card.Suits.SPADES, Card.Ranks.FIVE);
+		b = new Card(Card.Suits.SPADES, Card.Ranks.FIVE);
 
-		int aVal = a.getRank().ordinal() + 1;
-		int bVal = b.getRank().ordinal() + 1;
-
-		Assert.assertEquals(a.compareTo(b) > 0,aVal > bVal);
-		Assert.assertEquals(a.compareTo(b) == 0, aVal == bVal);
-		Assert.assertEquals(a.compareTo(b) < 0, aVal < bVal);
-
+		Assert.assertEquals(0,a.compareTo(b));
 	}
 
-	@AfterClass
-	public static void tearDown() {
-		Card.resetSuitIndex();
+	@Test
+	public void compareToGreater() {
+		a = new Card(Card.Suits.SPADES, Card.Ranks.QUEEN);
+		b = new Card(Card.Suits.SPADES, Card.Ranks.ACE);
+
+		Assert.assertEquals(11,a.compareTo(b));
 	}
+
 }
