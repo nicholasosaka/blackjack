a1cba80847f4694b36a1626616e545fdca5a8e9e
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-20 13:34:07 -0400

    scaffold deck test class

---
+++ tests/DeckTest.java
@@ -0,0 +1,43 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DeckTest {
+
+	@BeforeClass
+	public static void setUp(){
+
+
+	}
+
+	@Test
+	public void getDeck() {
+	}
+
+	@Test
+	public void addSingle() {
+	}
+
+	@Test
+	public void addMultiple() {
+	}
+
+	@Test
+	public void dealSingle() {
+	}
+
+	@Test
+	public void dealMultiple() {
+	}
+
+	@Test
+	public void populateDeck() {
+	}
+
+	@Test
+	public void shuffleDeck() {
+	}
+
+	@Test
+	public void toStringDeck() {
+	}
+}
