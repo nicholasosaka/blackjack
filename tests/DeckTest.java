ea23771f0674e77c0f8e514da34bcda9aab38969
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-20 09:13:18 -0400

    scaffold test for Deck class

---
+++ tests/DeckTest.java
@@ -0,0 +1,55 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DeckTest {
+	private static Deck deck;
+
+	@BeforeClass
+	public void setUpClass() {
+		deck = new Deck(true,2);
+	}
+
+	@Test
+	public void getDeck() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void addSingle() {
+		Assert.fail("Not yet implemented");
+
+	}
+
+	@Test
+	public void addMultiple() {
+		Assert.fail("Not yet implemented");
+
+	}
+
+	@Test
+	public void dealSingle() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void dealMultiple() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void populateDeck() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void shuffleDeck() {
+		Assert.fail("Not yet implemented");
+	}
+
+	@Test
+	public void toStringDeckTest() {
+		Assert.fail("Not yet implemented");
+
+	}
+}
