feeb45ed3a0e8a23b5c966c4a24a8fb4db99f8ba
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-20 13:52:15 -0400

    cause non-built tests to fail

--- tests/DeckTest.java
+++ tests/DeckTest.java
@@ -1,11 +1,14 @@
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class DeckTest {
 
 	@BeforeClass
 	public static void setUp(){
-
+		deck = new Deck(true, 2);
 
 	}
 
@@ -15,29 +18,36 @@ public class DeckTest {
 
 	@Test
 	public void addSingle() {
+		Assert.fail("Not yet implemented");
 	}
 
 	@Test
 	public void addMultiple() {
+		Assert.fail("Not yet implemented");
 	}
 
 	@Test
 	public void dealSingle() {
+		Assert.fail("Not yet implemented");
 	}
 
 	@Test
 	public void dealMultiple() {
+		Assert.fail("Not yet implemented");
 	}
 
 	@Test
 	public void populateDeck() {
+		Assert.fail("Not yet implemented");
 	}
 
 	@Test
 	public void shuffleDeck() {
+		Assert.fail("Not yet implemented");
 	}
 
 	@Test
 	public void toStringDeck() {
+		Assert.fail("Not yet implemented");
 	}
 }
