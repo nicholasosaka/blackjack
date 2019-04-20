86cfe8d671367ed9ec18334175d32745e3c107ef
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-19 22:16:03 -0400

    remove arquillian parts

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -1,24 +1,12 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-@RunWith(Arquillian.class)
 public class CardTest {
-	@Deployment
-	public static JavaArchive createDeployment() {
-		return ShrinkWrap.create(JavaArchive.class)
-				.addClass(Card.class)
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-	}
 
-	@org.junit.Test
+	@Test
 	public void getValue() {
 	}
 
-	@org.junit.Test
+	@Test
 	public void compareTo() {
 	}
 }
