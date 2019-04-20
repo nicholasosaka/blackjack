e5ac8e1c54e5b876c99872a99094a8c2c4d57f3d
Nikko Osaka <nikko.osaka99@gmail.com>
2019-04-20 09:09:13 -0400

    build tests for toString and getValue

--- tests/CardTest.java
+++ tests/CardTest.java
@@ -1,9 +1,11 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CardTest {
 	private static Card a;
 	private static Card b;
+	private static Card ace;
 
 
 	@Test
@@ -30,4 +32,31 @@ public class CardTest {
 		Assert.assertEquals(11,a.compareTo(b));
 	}
 
+	@Before
+	public void setUp() {
+		a = new Card(Card.Suits.DIAMONDS, Card.Ranks.FIVE);
+		b = new Card(Card.Suits.HEARTS, Card.Ranks.JACK);
+		ace = new Card(Card.Suits.SPADES, Card.Ranks.ACE);
+
+	}
+
+	@Test
+	public void getValueAce() {
+		Assert.assertEquals(11, ace.getValue());
+	}
+
+	@Test
+	public void getValueFace() {
+		Assert.assertEquals(10, b.getValue());
+	}
+
+	@Test
+	public void getValueNumeralCard() {
+		Assert.assertEquals(5, a.getValue());
+	}
+
+	@Test
+	public void toStringTest() {
+		Assert.assertEquals("A♠", ace.toString());
+	}
 }
